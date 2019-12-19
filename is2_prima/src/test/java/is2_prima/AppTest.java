93f60f64bf2d68417d62b40ba6dc9b08bee70033
Ricardo Serrano <rxsh964@gmail.com>
2019-12-19 13:47:26 -0500

    modifying unit tests

--- is2_prima/src/test/java/is2_prima/AppTest.java
+++ is2_prima/src/test/java/is2_prima/AppTest.java
@@ -1,17 +1,17 @@
 package is2_prima;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class AppTest {
-
+	
 	@Test
 	public void test() {
-		App.mainPrimaValue(18, 20);
-		App.mainPrimaValue(17, 20);
-		App.mainPrimaValue(20, 25);
-		App.mainPrimaValue(20, 0);
-		App.mainPrimaValue(91, 30);
-		App.mainPrimaValue(30, 31);
+		assertEquals("Prima value", App.mainPrimaValue(18, 20), 1400.0, 0);
+		assertEquals("Prima value", App.mainPrimaValue(17, 20), 0.0, 0);
+		assertEquals("Prima value", App.mainPrimaValue(91, 30), 0.0, 0);
+		assertEquals("Prima value", App.mainPrimaValue(30, 31), 0.0, 0);
 	}
 
 }
